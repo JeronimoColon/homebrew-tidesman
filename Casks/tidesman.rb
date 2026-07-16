4918fe25c46dff52df889bd65deb2026f89b4852
Jeronimo Colon III <625412+JeronimoColon@users.noreply.github.com>
2026-07-16 16:40:09 -0400

    tidesman 0.2.0

--- Casks/tidesman.rb
+++ Casks/tidesman.rb
@@ -1,6 +1,6 @@
 cask "tidesman" do
-  version "0.1.3"
-  sha256 "dae589029cd11a91f28c07375868cf99069a35cb8317557fc5753325165bcfeb"
+  version "0.2.0"
+  sha256 "292590fbdb5d7898cc402317af64bd618e40af1cdae749a20b8c1d19074257d4"
 
   url "https://github.com/JeronimoColon/tidesman-mcp/releases/download/v#{version}/tidesman-#{version}-macos-arm64.zip",
       verified: "github.com/JeronimoColon/tidesman-mcp/"
